f455fec0031d835c9080398bfbbd6e8fc5dd3e0b
zubayer <zubayer3570@gmail.com>
2023-04-23 03:11:37 +0600

    hi

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYpGzo1NtTrQxwOOpGpAS6AeeuHOd9Qtg",
+  authDomain: "soundex-52cdd.firebaseapp.com",
+  projectId: "soundex-52cdd",
+  storageBucket: "soundex-52cdd.appspot.com",
+  messagingSenderId: "752967862651",
+  appId: "1:752967862651:web:12b49ee2e59dd8d396d5de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
