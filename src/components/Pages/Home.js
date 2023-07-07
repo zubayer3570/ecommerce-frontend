9dd56fccb6be32dcdddf69f028141ceef322e823
zubayer <zubayer3570@gmail.com>
2023-07-08 01:26:06 +0600

    final-2

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -6,9 +6,7 @@ import ProductCard from '../main-components/ProductCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateFooter } from '../../features/footerSlice';
 import { FacebookProvider, CustomChat } from 'react-facebook'
-import { auth } from '../../init.firebase';
 import AllProducts from '../main-components/AllProducts';
-import Spinner from '../main-components/Spinner';
 
 const Home = () => {
     const dispatch = useDispatch()
@@ -20,9 +18,9 @@ const Home = () => {
             <TopProducts />
             <p className='text-[30px] font-bold text-3 text-center mt-12 mb-6'>All Products</p>
             <AllProducts />
-            {/* <FacebookProvider appId='1369564030289731'>
+            <FacebookProvider appId='1369564030289731'>
                 <CustomChat pageId='101280129700771' />
-            </FacebookProvider> */}
+            </FacebookProvider>
         </>
     );
 };
